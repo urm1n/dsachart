bd381ea600a16a160471ead428b5947f919b4719
urm1n <urminhirpara@gmail.com>
2025-05-17 18:23:45 +0530

    refactor(next.config): remove basePath configuration
    The basePath configuration was commented out to simplify the deployment process and avoid potential routing issues. This change ensures that the application can be served from the root path without requiring a specific base path.

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     images: {
       unoptimized: true,
     },
-    basePath: '/dsachart',
+    // basePath: '/dsachart',
     eslint: {
       ignoreDuringBuilds: true,
     },
